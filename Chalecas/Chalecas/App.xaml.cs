230c28e8a2fd9789c2225ddfacfe9c243711c50f
Ed <pushpoped@gmail.com>
2021-11-23 21:51:13 -0500

    adding git ignore

--- Chalecas/Chalecas/App.xaml.cs
+++ Chalecas/Chalecas/App.xaml.cs
@@ -10,7 +10,7 @@ namespace Chalecas
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage( new MainPage());
         }
 
         protected override void OnStart()
--- Chalecas/Chalecas/MainPage.xaml.cs
+++ Chalecas/Chalecas/MainPage.xaml.cs
@@ -1,18 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Chalecas.ViewModels;
+using Chalecas.Views;
 using Xamarin.Forms;
 
 namespace Chalecas
 {
     public partial class MainPage : ContentPage
     {
+        private MainViewModel _viewModel;
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = _viewModel = new MainViewModel();                        
+        }
+
+        async void btn1_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new MyPageTwo());
         }
     }
 }
