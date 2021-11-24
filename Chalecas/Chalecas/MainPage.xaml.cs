849dc9b30fd2c29bcfea5e48e709f2b046f7f4cc
Ed <pushpoped@gmail.com>
2021-11-23 21:57:15 -0500

    Removing missing VM

--- Chalecas/Chalecas/MainPage.xaml.cs
+++ Chalecas/Chalecas/MainPage.xaml.cs
@@ -1,21 +1,20 @@
-﻿using Chalecas.ViewModels;
-using Chalecas.Views;
+﻿]
 using Xamarin.Forms;
 
 namespace Chalecas
 {
     public partial class MainPage : ContentPage
     {
-        private MainViewModel _viewModel;
+        
         public MainPage()
         {
             InitializeComponent();
-            BindingContext = _viewModel = new MainViewModel();                        
+                                 
         }
 
         async void btn1_Clicked(System.Object sender, System.EventArgs e)
         {
-            await Navigation.PushAsync(new MyPageTwo());
+            
         }
     }
 }
