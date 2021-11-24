d604b7093cef90e37408c41d1ad8871113f3a109
Ed <pushpoped@gmail.com>
2021-11-23 21:56:05 -0500

    Folders & BaseViewModel added

---
+++ Chalecas/Chalecas/ViewModels/BaseViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Chalecas.ViewModels
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged // interfaces implemented
+    {
+        #region props
+        private string _title;
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                _title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+        #endregion
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+
+        public BaseViewModel()
+        {
+        }
+
+    }
+}
